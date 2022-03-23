b511c0bde038c43f0be37cf96c59fe099bf7c9e3
Javier_GR <javigallego99@gmail.com>
2022-03-23 21:52:36 +0100

    build removed from gitignore

---
+++ client/build/precache-manifest.50ceb3fac467f73ef4d0d63aae422edb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4e085a50c27616408aed3bde784dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9cc06159afdc118aee",
+    "url": "/static/css/2.bb65b29e.chunk.css"
+  },
+  {
+    "revision": "ab9cc06159afdc118aee",
+    "url": "/static/js/2.6a6829d9.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.6a6829d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cd0cabd3eddb397225",
+    "url": "/static/js/main.149c9150.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  },
+  {
+    "revision": "b0dc21936e27b98d6c65bd72dd705c65",
+    "url": "/static/media/bootstrap-icons.b0dc2193.woff"
+  },
+  {
+    "revision": "c5787b4ab55835580294696c3cbcc521",
+    "url": "/static/media/bootstrap-icons.c5787b4a.woff2"
+  },
+  {
+    "revision": "b3cd613acc83bf8756f519f2dc090e65",
+    "url": "/static/media/logo-olecosta.b3cd613a.svg"
+  }
+]);
